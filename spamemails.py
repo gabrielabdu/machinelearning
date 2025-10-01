fe49d978473e80bba7d96105fc5fd563d3a18dfa
gabrielabdu <gabrieltabdu@gmail.com>
2025-10-01 19:48:50 -0300

    Create spamemails.py

---
+++ spamemails.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import string
+import nltk
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+nltk.download('stopwords')
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+import warnings
+warnings.filterwarnings('ignore')
